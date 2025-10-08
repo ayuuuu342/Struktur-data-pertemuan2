95d0084f342cfed6d118c5e0bb109f2dd5dc9135
Ayu Setyaning Tyas <ayusetyaningtyas13@gmail.com>
2025-10-08 16:18:43 +0700

    Add files via upload

---
+++ guided4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int maks3(int a, int b, int c);
+
+int main(){
+    int x, y, z;
+    cout << "masukkan nilai bilangan ke-1   = ";
+    cin >> x;
+    cout << "masukkan nilai bilangan ke-2   = ";
+    cin >> y;
+    cout << "masukkan nilai bilangan ke-3   = ";
+    cin >> z;
+
+    cout << "nilai maksimumnya adalah = " << maks3(x, y, z);
+    return 0;
+}
+
+//badan fungsi
+int maks3(int a, int b, int c){
+    int temp_max = a;
+    if (b > temp_max)
+        temp_max = b;
+    if (c > temp_max)
+        temp_max = c;
+    return temp_max;   
+}
